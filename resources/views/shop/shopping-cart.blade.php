ba05b3f9b1e2ed098e598da502624ac5c0fde26f
royyy <roy0072809@gmail.com>
2020-05-28 18:30:01 +0800

    伸縮編輯
    圖片搞不出來

--- resources/views/shop/shopping-cart.blade.php
+++ resources/views/shop/shopping-cart.blade.php
@@ -5,30 +5,35 @@
     <meta charset="UTF-8">
     <title>購物車</title>
     <link rel="stylesheet" href="/css/購物車.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script>
+    $(document).ready(function(){
+        $("li").hide();
+        $("ul").click(function(){
+            $("li").toggle();
+        });
+    });
+
+</script>
 </head>
 
 <body>
     @if(Session::has('cart'))
         <div class="container">
             <div class="item_header">
-                <div class="name">商品</div>
-                <div class="price">金額</div>
-                <div class="count">數量</div>
-                <div class="operate">編輯</div>
+                <div class="name" style="width:200px">商品</div>
+                <div class="price" style="width:200px">金額</div>
+                <div class="count" style="width:260px">數量</div>
+                <div class="delete" style="width:200px">編輯</div>
             </div>
             @foreach($products as $product)
-                <div class="item_container">
+                <div class="container">
                     <div class="item_header item_body">
                         <div class="name">{{$product['item']['title']}}</div>
                         <div class="price"><span>$</span>{{$product['price']}}</div>
                         <div class="count">{{$product['qty']}}</div>
-                        <div class="operate">
-                            <button type="button" class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown">
-                                編輯 <span class="caret"></span>
-                            </button>
-                        </div>
                         <div class="delete">
-                            <ul class="dropdown-menu">
+                            <ul>點這裡編輯
                                 <li><a href="{{route('product.reduceByOne', ['id' => $product['item']['id']])}}">數量減一</a></li>
                                 <li><a href="{{route('product.remove', ['id' => $product['item']['id']])}}">刪除</a></li>
                             </ul>
@@ -38,8 +43,8 @@
             @endforeach
             <div class="item_container">
                 <div class="item_header item_body">
-                    <div style="width:450px;text-align:right"><strong>總計金額 : ${{$totalPrice}}{{--總金額--}}</strong></div>
-                    <div style="width:400px;text-align:right"><a href="{{route('checkout')}}" type="button" class="btn btn-success">結帳</a></div>
+                    <div style="width:240px;text-align:right"><strong>總計金額 : ${{$totalPrice}}{{--總金額--}}</strong></div>
+                    <div style="width:460px;text-align:right"><a href="{{route('checkout')}}" type="button" class="btn btn-success">結帳</a></div>
                 </div>
             </div>
             <div style="width:100%;text-align:center" class="btn btn-primary">
@@ -48,6 +53,40 @@
                 </button>
             </div>
         </div>
+        {{-- <div class="row">
+            <div class="col-sm-6 col-md-6 col-md-offset-3 col-sm-offset-3">
+                <ul class="list-group">
+                    @foreach($products as $product)
+                        <li class="list-group-item">
+                             <span class="badge">{{$product['qty']}}</span>
+                            <strong>{{$product['item']['title']}}</strong>
+                            <span class="label label-success">{{$product['price']}}</span>
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown">
+                                    編輯 <span class="caret"></span>
+                                </button>
+
+                                <ul class="dropdown-menu">
+                                    <li><a href="{{route('product.reduceByOne', ['id' => $product['item']['id']])}}">Reduce by 1</a></li>
+                                    <li><a href="{{route('product.remove', ['id' => $product['item']['id']])}}">Reduce All</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6 col-md-6 col-md-offset-3 col-sm-offset-3">
+                <strong>Total: {{$totalPrice}}</strong>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-sm-6 col-md-6 col-md-offset-3 col-sm-offset-3">
+                <a href="{{route('checkout')}}" type="button" class="btn btn-success">Checkout</a>
+            </div>
+        </div> --}}
     @else
         <center>
             <div class="container">
