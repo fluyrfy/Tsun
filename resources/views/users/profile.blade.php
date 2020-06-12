f4df59531713c4db43993506944896473318aa26
royyy <roy0072809@gmail.com>
2020-06-12 11:03:06 +0800

    訂單查詢路徑

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -11,7 +11,7 @@
             <p>
             <a href="編輯個人資料.html"><input type="button" value="編輯個人資料" style="width:120px;height:40px;border:2px #9999FF dashed;background-color:white;"></a>
             <p>
-            <a href="訂單查詢.html"><input type="button" value="訂單查詢" style="width:120px;height:40px;border:2px #9999FF dashed;background-color:white;"></a>
+            <a href="{{ route('pages.orderin')}}"><input type="button" value="訂單查詢" style="width:120px;height:40px;border:2px #9999FF dashed;background-color:white;"></a>
             <p>
             <a href="登出.html"><input type="button" value="登出" style="width:120px;height:40px;border:2px #9999FF dashed;background-color:white;"></a>
         </form>
